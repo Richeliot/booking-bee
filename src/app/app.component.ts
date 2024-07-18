2227c3fd6214b8d61c19f21ccdee9f9993fb2a6d
Richard Tezbir <tezbir@huglo.com>
2024-07-18 06:39:11 +0200

    bootstrap, material theme 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from "./menu/menu.component";
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
+
+/* 
+
+HYDRATION
+  Very quickly, 
+  easily,
+  intuitively,
+  and declaratively
+  specify portions of apps.
+  
+*/
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  styleUrl: './app.component.less',
 })
 export class AppComponent {
-  title = 'booking';
+
+  constructor(private modalService: NgbModal) {}
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
 }
